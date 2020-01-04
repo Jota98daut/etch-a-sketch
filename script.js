8e7c1a04ff7f2c2746fb7bced9599346720e8d84
Joel D'Autilio <joeldautilio@gmail.com>
2020-01-04 04:01:44 -0300

    First stable commit

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 let container = document.querySelector('.container');
+let gridSize = 16;
 
-for (let i = 0; i < 16; i++) {
-    let divi = document.createElement('div');
-    for (let j = 0; j < 16; j++) {
-        let divj = document.createElement('div');
-        divj.style.border = '1px solid grey';
-        divi.appendChild(divj);
+// Create grid of size gridSize
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let divi = document.createElement('div');
+        divi.classList.add('firstLevel');
+        divi.style.height = `${100/size}%`;
+        for (let j = 0; j < size; j++) {
+            let divj = document.createElement('div');
+            divj.classList.add('secondLevel');
+
+            divj.addEventListener('mouseover', () => {
+                divj.style.backgroundColor = 'orange';
+            });
+
+            divi.appendChild(divj);
+        }
+        container.appendChild(divi);
     }
-    container.appendChild(divi);
 }
+
+// Deletes an existing grid
+function deleteGrid() {
+    let parent = document.querySelector('.container');
+    let childs = document.querySelectorAll('.firstLevel');
+
+    childs.forEach(child => parent.removeChild(child));
+}
+
+generateGrid(gridSize);
+
+// Button to generate grid
+let gen = document.querySelector('#generator');
+
+gen.addEventListener('click', () => {
+    let childs = document.querySelectorAll('.firstLevel');
+    
+    let newSize = prompt('Size (in squares per side):');
+    gridSize = +newSize;
+    
+    if (typeof(gridSize) !== 'number' || !gridSize) return 0;
+    if(childs) deleteGrid();
+
+    generateGrid(gridSize);
+});
